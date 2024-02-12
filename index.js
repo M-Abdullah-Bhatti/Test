fb938af68361702788bd5f43ff5b16462dbd1769
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2024-02-12 20:51:43 +0500

    about page added

--- index.js
+++ index.js
@@ -3,3 +3,82 @@ console.log("Hello");
 console.log("Hello");
 
 console.log("Hello");
+
+const allAboutUsArray = [
+  {
+    id: 1,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 2,
+    name: "KATTIE TROTTER",
+    designation: "FASHION DIRECTOR, VOGUE ARABIA",
+    content:
+      "“From the vast Sahara Desert to the stunning Red Sea coast, Egypt radiates beauty through a diverse tapestry of its people, cultural heritage, and captivating landscapes. We at Vogue are so impressed by your big hearts and determination to do the best job possible at Snap14, and want to thank every single one of you for your hard work and dedication”",
+  },
+  {
+    id: 3,
+    name: "RULA TAYARA",
+    designation: "CREATIVE SOLUTIONS PROJECT MANAGER, HARPER’S BAZAAR ARABIA",
+    content:
+      "“The team at Snap14 were very hands-on and professional... the photo turned out to be one of the best from the issue”",
+  },
+  {
+    id: 4,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 5,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 6,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 7,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 8,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 9,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+  {
+    id: 10,
+    name: "JESSICA DIXON",
+    designation: "DIRECTOR OF PRODUCT DEVELOPMENET & PRODUCTION SEE.MAS",
+    content:
+      "“Collaborative from onset with focus on client’s Brand needs and executing agreed deliverables at a very competitive cost with high-quality standards. Modern and easy to work with! I look forward to working with Snap 14 Agency in the future”.",
+  },
+];
+
+let showItem = 3;
+
+function viewMore() {
+  showItem += 2;
+}
